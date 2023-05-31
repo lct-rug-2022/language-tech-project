596428801d450ab25c57b0301ccf459badee83af
saranabhani <sara.nabhani.sn@gmail.com>
2023-05-31 17:02:36 +0200

    feat: add pred script

---
+++ discussion-task/predict.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --time=2:00:00
+#SBATCH --partition=gpushort
+#SBATCH --gpus-per-node=a100.20gb:1
+#SBATCH --job-name=values-task
+#SBATCH --mem=20G
+
+
+module purge
+module load CUDA/11.7.0
+module load cuDNN/8.4.1.50-CUDA-11.7.0
+module load NCCL/2.12.12-GCCcore-11.3.0-CUDA-11.7.0
+module load Python/3.10.4-GCCcore-11.3.0
+module load GCC/11.3.0
+
+
+source .venv/bin/activate
+
+
+python discussion-task/predict.py $*
+
+
+deactivate
